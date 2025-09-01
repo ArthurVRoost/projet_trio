1faecae9e91b7530c4ac597e9a9a4d3c6aa3c509
Arthur Van Roost <arthur.vanroost@icloud.com>
2025-09-01 16:15:06 +0200

    homecontroller

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Continent;
+use App\Models\Equipe;
+use App\Models\Genre;
+use App\Models\Joueur;
+use App\Models\Position;
+use App\Models\Role;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function home(){
+        $equipes = Equipe::all();
+        $genres = Genre::all();
+        $continents = Continent::all();
+        $joueurs = Joueur::all();
+        $positions = Position::all();
+        $roles = Role::all();
+        return view('welcome', compact('equipes', 'genres', 'continents', 'joueurs', 'positions', 'roles'));
+    }
+}
