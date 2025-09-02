e079097c0a9840642c7aa6fa592a4d1660dcb6d1
Kamil Baldyga <baldygakamil@gmail.com>
2025-09-02 13:42:27 +0200

    ajout de index, edit, update et destroy dans UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,4 +11,19 @@ class UserController extends Controller
         $users = User::all();
         return view("users.index", compact("users"));
     }
+    public function edit($id) {
+        $user = User::find($id);
+        return view("users.edit", compact("user"));
+    }
+    public function update(Request $request, $id) {
+        $user = User::find($id);
+        $user->role_id = $request->role_id;
+        $user->save();
+        return redirect()->route("users.update")->with("success","Le rôle de l'utilisateur a été modifié");
+    }
+    public function destroy($id) {
+        $user = User::find($id);
+        $user->delete();
+        return redirect()->back()->with("success","L'utilisateur a été supprimé");
+    }
 }
