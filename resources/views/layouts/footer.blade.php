5fc95c18d66fe09350f623776601b465701c96cd
Kamil Baldyga <baldygakamil@gmail.com>
2025-09-04 21:02:39 +0200

    ajout footer

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+    <div class="container">
+        <div class="footer-content">
+            Projet réalisé avec <i class="bi bi-heart-fill footer-heart"></i> et
+            <i class="bi bi-droplet-fill footer-blood"></i> par Charlotte, Arthur et Kamil
+        </div>
+    </div>
+</footer>
